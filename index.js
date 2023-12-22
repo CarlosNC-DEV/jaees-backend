a22be1e4b20f9154117de5c7ccee25e91d54ae6a
CarlosNC-DEV <carlosnogueradev@gmail.com>
2023-12-22 13:59:09 -0500

    add response default

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import app from './src/app.js';
 import { PORT } from './src/config.js';
 import './src/db.js'
 
+app.use((req, res, next) => {
+    return res.status(200).json("Bienvenido a JAEES");
+  });
+
 app.listen(PORT, ()=>{
     console.log("servidor en puerto", PORT)
 });
