0a1ec3514ad684d6b84d292e88cbafe9ab3a8cb1
CarlosNC-DEV <carlosnogueradev@gmail.com>
2024-01-10 21:44:44 -0500

    add validate time zone

--- src/controllers/lotteries.controller.js
+++ src/controllers/lotteries.controller.js
@@ -39,11 +39,14 @@ export const getAllLottieres = async (req, res) => {
 export const getAllLottieresHabilitadas = async (req, res) => {
   try {
 
-    const today = new Date().toLocaleDateString('es-US', { weekday: 'long' }).toLowerCase();
+    const timeZone = 'America/Bogota'; 
+    const options = { weekday: 'long' };
+    const now = new Date(new Date().toLocaleString('en-US', { timeZone }));
+    const formattedDate = now.toLocaleDateString('es-US', options).toLowerCase();
 
     const allLottieres = await LotteriesModel.find({state:true});
 
-    const lotteriesToday = allLottieres.filter(lottery => lottery.dayGames.includes(today));
+    const lotteriesToday = allLottieres.filter(lottery => lottery.dayGames.includes(formattedDate));
 
     return responseSuccess(res, 200, "loterias", lotteriesToday);
 
